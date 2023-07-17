4379c70d015e47381da5fd55f364a5a5403f2b97
Santiago Orjuela <santiago.orjuela@utp.edu.co>
2023-07-17 10:04:43 -0500

    first commit

---
+++ src/main/java/hotelSaver/com/hotelSaver/HotelSaverApplication.java
@@ -0,0 +1,13 @@
+package hotelSaver.com.hotelSaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelSaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelSaverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hotelSaver/com/hotelSaver/HotelSaverApplicationTests.java
@@ -0,0 +1,13 @@
+package hotelSaver.com.hotelSaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelSaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
